95bbe5092ebb7c5c0cf82745e3a6bdadbdb6c845
grammer <675476193@qq.com>
2018-04-25 22:54:02 +0800

    start: dilation & erosion oper.

---
+++ dilation_erosion_demo/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>  // Gaussian Blur
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>  // OpenCV window I/O
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char *argv[])
+{
+
+
+
+    return 0;
+}
